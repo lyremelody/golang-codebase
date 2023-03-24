887ed74b663cd86f01ace5910dbca3b86708cb73
lyremelody <luyuan87@gmail.com>
2023-03-24 08:57:34 +0800

    add 'developing a RESTful API with Go and Gin'

---
+++ 004-web-service-gin/web-service-gin/README.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+go mod init example/web-service-gin
+
+go get .
+
+go run . &
+
+sleep 10
+
+curl http://localhost:8080/albums
+
+curl http://localhost:8080/albums \
+    --include \
+    --header "Content-Type: application/json" \
+    --request "POST" \
+    --data '{"id": "4","title": "The Modern Sound of Betty Carter","artist": "Betty Carter","price": 49.99}'
+
+curl http://localhost:8080/albums
+
+curl http://localhost:8080/albums/2
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 * [001-getting-started](https://go.dev/doc/tutorial/getting-started)
 * [002-create-module](https://go.dev/doc/tutorial/create-module)
 * [003-workspaces](https://go.dev/doc/tutorial/workspaces)
+* [004-web-service-gin](https://go.dev/doc/tutorial/web-service-gin)
